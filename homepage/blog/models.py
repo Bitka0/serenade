149a548e907c7d2d35a4514b493eca4e32cafbc2
Phillip Thelen <phillip.thelen@junge-piraten.de>
2011-02-21 23:28:35 +0100

    beginning of the comment stuff

--- homepage/blog/models.py
+++ homepage/blog/models.py
@@ -7,6 +7,8 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
+
 
 class Group(models.Model):
 	name = models.CharField(max_length=200)
@@ -20,6 +22,20 @@ class Tag(models.Model):
 	def __unicode__(self):
 		return self.name
 
+class Comment(models.Model):
+	sender = models.CharField(max_length=80)
+	date = models.DateTimeField(auto_now=True)
+	homepage = models.CharField(max_length=200)
+	
+	subject = models.CharField(max_length=100)
+	comment = models.TextField()
+
+class CommentForm(forms.Form):
+	subject = forms.CharField(max_length=100)
+	message = forms.CharField()
+	sender = forms.CharField(max_length=100)
+	homepage = forms.CharField(max_length=200)
+	
 class Blogentry(models.Model):
 	url = models.SlugField('URL', max_length=200, help_text = 'This field is automatically filled based on the title you enter, however if you want to customize the URL, here you can.')
 
@@ -32,7 +48,7 @@ class Blogentry(models.Model):
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	
+	comment = models.ManyToManyField(Comment)
+	
 	def __unicode__(self):
 		return self.title
-
-
