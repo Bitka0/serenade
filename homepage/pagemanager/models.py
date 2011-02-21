9d57cb4948ea896893348374d199a2568ad761f2
Lukas Martini <lukas.martini@unionhost.de>
2011-02-21 18:09:26 +0100

    It's URL, not Url.

--- homepage/pagemanager/models.py
+++ homepage/pagemanager/models.py
@@ -8,7 +8,7 @@
 from django.db import models
 
 class Page(models.Model):
-	url = models.CharField(max_length=200)
+	url = models.CharField('URL', max_length=200)
 
 	title = models.CharField(max_length=200)
 	comment = models.TextField()
