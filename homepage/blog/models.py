ffa329ccfcd675c8e04bd04d3ded49f2619f99d6
Lukas Martini <lukas.martini@unionhost.de>
2011-02-21 23:57:31 +0100

    Comments now have a blog entry as ForeignKey; Translations

--- homepage/blog/models.py
+++ homepage/blog/models.py
@@ -31,7 +31,29 @@ class Tag(models.Model):
 		verbose_name = _('tag')
 		verbose_name_plural = _('tags')
 		
+class Blogentry(models.Model):
+	url = models.SlugField(_('URL'), max_length=200, help_text = _('This field is automatically filled based on the title you enter, however if you want to customize the URL, here you can.'))
+
+	created_by = models.ForeignKey(User, verbose_name = _('creator'))
+	publishingDate = models.DateTimeField(_('publication date'), auto_now_add=True)
+	lastModified = models.DateTimeField(_('date of last modification'), auto_now=True)
+	group = models.ManyToManyField(Group, verbose_name = _('groups'))
+	tag = models.ManyToManyField(Tag, verbose_name = _('tags'))
+
+	title = models.CharField(_('title'), max_length=200)
+	text = models.TextField(_('text'))
+	
+	comment = models.ManyToManyField(Comment, verbose_name = _('comments'))
+	
+	def __unicode__(self):
+		return self.title
+
+	class Meta:
+		verbose_name = _('blogentry')
+		verbose_name_plural = _('blogentries')
+
 class Comment(models.Model):
+	blogentry = models.ForeignKey(BlogEntry, verbose_name = _('related blog entry'))
 	sender = models.CharField(_('author'), max_length=80)
 	date = models.DateTimeField(_('date of creation'), auto_now=True)
 	homepage = models.CharField(_('homepage'), max_length=200)
@@ -48,25 +70,3 @@ class CommentForm(forms.Form):
 	message = forms.CharField()
 	sender = forms.CharField(max_length=100)
 	homepage = forms.CharField(max_length=200)
-
-
-class Blogentry(models.Model):
-	url = models.SlugField(_('URL'), max_length=200, help_text = _('This field is automatically filled based on the title you enter, however if you want to customize the URL, here you can.'))
-
-	created_by = models.ForeignKey(User)
-	publishingDate = models.DateTimeField(_('publication date'), auto_now_add=True)
-	lastModified = models.DateTimeField(_('date of last modification'), auto_now=True)
-	group = models.ManyToManyField(Group)
-	tag = models.ManyToManyField(Tag)
-
-	title = models.CharField(_('title'), max_length=200)
-	text = models.TextField(_('text'))
-	
-	comment = models.ManyToManyField(Comment)
-	
-	def __unicode__(self):
-		return self.title
-
-	class Meta:
-		verbose_name = _('blogentry')
-		verbose_name_plural = _('blogentries')
