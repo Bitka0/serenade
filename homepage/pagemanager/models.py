ddcaacd4e97d52b7c2765237740b1a402e75b735
Lukas Martini <lukas.martini@unionhost.de>
2011-02-21 17:46:37 +0100

    Let there be license.

--- homepage/pagemanager/models.py
+++ homepage/pagemanager/models.py
@@ -1,3 +1,10 @@
+# coding: utf-8 
+
+# Copyright (c) 2011 Lukas Martini, Phillip Thelen.
+# This file may be used and distributed under the terms found in the
+# file COPYING, which you should have received along with this
+# program. If you haven't, please refer to bofh@junge-piraten.de.
+
 from django.db import models
 
 class Page(models.Model):
