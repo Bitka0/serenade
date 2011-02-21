7ea472b2357ebc2a6f239b1ac32280806f726a41
Lukas Martini <lukas.martini@unionhost.de>
2011-02-22 00:14:35 +0100

    Add header to homepage/__init__.py

--- homepage/__init__.py
+++ homepage/__init__.py
@@ -1,3 +1,10 @@
+# coding: utf-8 
+
+# Copyright (c) 2011 Lukas Martini, Phillip Thelen.
+# This file may be used and distributed under the terms found in the
+# file COPYING, which you should have received along with this
+# program. If you haven't, please refer to bofh@junge-piraten.de.
+
 from django.utils.translation import ugettext_lazy as _
 
 _(u'pagemanager')
