871af6ccc5792d685a97bc954fe28ad8d815c13d
Lukas Martini <lukas.martini@unionhost.de>
2011-02-22 00:50:11 +0100

    Fix commenting

--- homepage/blog/views.py
+++ homepage/blog/views.py
@@ -5,36 +5,36 @@
 # file COPYING, which you should have received along with this
 # program. If you haven't, please refer to bofh@junge-piraten.de.
 
+from django.utils.translation import ugettext_lazy as _
 from django.template import Context, loader
 from models import Blogentry, Group, Tag, Comment, CommentForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
-from django.core.urlresolvers import reverse
 from django import forms
 import util
 
 def listAll(request):
-
 	entrylist = Blogentry.objects.all().order_by('-publishingDate')
-	context = util.generateContext(request, entries = entrylist)
+	context = util.generateContext(request, title = _('Blog'), entries = entrylist)
 	return render_to_response('blog/standard.html', context)
 
 def show(request, url):
 	url = util.stripSlash(url)
-
-	if url == '' or url == None:
-		url = '/'
+	entry = get_object_or_404(Blogentry, url = url)
 		
 	if request.method == 'POST':
-		form = CommentForm(request.POST) # A form bound to the POST data
-		if form.is_valid(): # All validation rules pass
-			# Process the data in form.cleaned_data
-			# ...
-				return HttpResponseRedirect('/thanks/') # Redirect after POST
-	else:
-		form = CommentForm() # An unbound form
+		form = CommentForm(request.POST)
 		
-	entry = get_object_or_404(Blogentry, url=url)
-	context = util.generateContext(request, title = entry.title, text = entry.text, url = entry.url, groups = entry.group.all(), tags = entry.tag.all(), pubDate = entry.publishingDate, created_by = entry.created_by, commentform = form, comments = entry.comment)
+		if form.is_valid():
+			comment = Comment(blogentry = entry)
+			comment.subject = form.cleaned_data['subject']
+			comment.message = form.cleaned_data['message']
+			comment.sender = form.cleaned_data['sender']
+			comment.homepage = form.cleaned_data['homepage']
+			comment.save()	
+	else:
+		form = CommentForm()
+		
+	context = util.generateContext(request, contextType = 'RequestContext', title = entry.title, entry = entry, groups = entry.group.all(), tags = entry.tag.all(), commentform = form, comments = Comment.objects.filter(blogentry = entry))
 	return render_to_response('blog/show.html', context)
 
